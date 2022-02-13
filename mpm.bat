96b4f56a345890c519935c369951b4e6131e1754
TTAK <pitchaimbox-github@yahoo.com>
2022-02-13 10:50:54 +0530

    hiall

--- mpm.bat
+++ mpm.bat
@@ -4,4 +4,5 @@ cd %~dp0
 ::start D:\MPM_Dev_Center\PortableJekyll-master\setpath.cmd
 ::bundle exec jekyll build -incremental
 ::jekyll serve -incremental
-bundle exec jekyll build & cd %~dp0\_site & GIT add . & git commit -am "hi all" & GIT PUSH
+bundle exec jekyll build & cd %~dp0\_site 
+GIT add . & git commit -am hiall & GIT PUSH
