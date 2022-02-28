8ba96b7f41a1667c77bdcdb6fb4d909b076c08c8
unknown <pitchaimbox-github@yahoo.com>
2022-02-28 23:00:36 +0530

    hiall

--- mpm.bat
+++
@@ -1,8 +0,0 @@
-cd /
-rem /*
-cd %~dp0
-::start D:\MPM_Dev_Center\PortableJekyll-master\setpath.cmd
-::bundle exec jekyll build -incremental
-::jekyll serve -incremental
-bundle exec jekyll build & cd %~dp0\_site 
-GIT add . & git commit -am hiall & GIT PUSH
