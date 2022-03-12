be0d9707483cfff30c084c37c13ed8912c94df58
thanithamizhakarathikalanjiyam <pitchaimbox-github@yahoo.com>
2022-03-13 00:43:54 +0530

    dsf

--- mpm.bat
+++ mpm.bat
@@ -1,8 +1,6 @@
-cd /
-rem /*
+::cd /
+::rem /*
 cd %~dp0
-::start D:\MPM_Dev_Center\PortableJekyll-master\setpath.cmd
-::bundle exec jekyll build -incremental
-::jekyll serve -incremental
-bundle exec jekyll build & cd %~dp0\_site 
-GIT add . & git commit -am hiall & GIT PUSH
+start D:\MPM_Dev_Center\PortableJekyll-master\setpath.cmd
+jekyll --version
+bundle exec jekyll build & cd %~dp0\_site & GIT add . & git commit -am hiall & GIT PUSH
