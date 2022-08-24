320e3c3363809c1db8f3136062c009b33ffab7e5
mpm <pitchaimbox-interview@yahoo.co.in>
2022-08-24 21:46:12 +0530

    hiall

--- mpm.bat
+++ mpm.bat
@@ -1,8 +1,7 @@
 ::cd /
 ::rem /*
 cd %~dp0
-D:\MPM_Dev_Center\PortableJekyll-master\setpath-ry31.cmd 
-jekyll --version
-jekyll build -d D:\GitRepo\thanithamizhakarathikalanjiyam.github.io
+D:\MPM_Dev_Center\PortableJekyll-master\setpath-ry31.cmd jekyll --version
+bundle exec jekyll build -d D:\GitRepo\thanithamizhakarathikalanjiyam.github.io
 ::cd %~dp0\_site & GIT add . & git commit -am hiall & GIT PUSH
 cd D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & GIT add . & git commit -am hiall & GIT PUSH
