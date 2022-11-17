29e370eb7e7ead1089db02cb7bff79e9840e5ebd
Pitchaimuthu <pitchaimbox-github.yahoo.com>
2022-11-17 11:14:16 +0530

    dfs

--- mpm.bat
+++ mpm.bat
@@ -2,3 +2,4 @@
 ::rem /*
 cd %~dp0
 D:\GitRepo\PortableJekyll-master\setpath-ry31.cmd  bundle exec jekyll build -d D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & cd D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & GIT add . & git commit -am hiall & GIT PUSH
+bundle exec jekyll build -d /media/muthu/d/GitRepo/thanithamizhakarathikalanjiyam.github.io
