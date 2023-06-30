039f2a4875a0ee1f1d392c7be1375a3bda84573c
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2023-06-30 08:46:20 +0530

    morn

--- mpm.bat
+++ mpm.bat
@@ -3,3 +3,4 @@
 ::cd %~dp0
 ::D:\GitRepo\PortableJekyll-master\setpath-ry31.cmd  bundle exec jekyll build -d D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & cd D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & GIT add . & git commit -am hiall & GIT PUSH
 bundle exec jekyll build -d ../thanithamizhakarathikalanjiyam.github.io
+bundle exec jekyll serve -d ../thanithamizhakarathikalanjiyam.github.io
