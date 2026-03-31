5596b7e303cc76b2479cf417967fcb82676c79d9
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2026-03-31 19:39:43 +0530

    test

--- mpm - Copy (2).bat
+++
@@ -1,4 +0,0 @@
-::cd /
-::rem /*
-cd %~dp0
-D:\MPM_Dev_Center\PortableJekyll-master\setpath-ry31.cmd  bundle exec jekyll build -d D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & cd D:\GitRepo\thanithamizhakarathikalanjiyam.github.io & GIT add . & git commit -am hiall & GIT PUSH
